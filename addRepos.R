778ff90459c587702be12bb9ddac0151dcaa784d
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2018-09-30 15:46:50 -0700

    addRepos.R

--- addRepos.R
+++ addRepos.R
@@ -4,3 +4,8 @@ options(dratBranch="master")
 
 drat::insertPackage("../osmose_0.1.1.9001.zip", ".")
 drat::insertPackage("../osmose_0.1.1.9001.tar.gz", ".")
+
+
+install.packages("osmose", repo="https://osmose-model.github.io/drat/",
+                 dependencies = TRUE)
+
