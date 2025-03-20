40d8d127ffa63e079872ecc570ba0cb71ab49049
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2025-03-20 05:30:00 +0100

    update repo command

--- add_packages.R
+++ add_packages.R
@@ -13,6 +13,8 @@ other = setdiff(nother, all_contrib)
 if(length(own)!=0) drat::insertPackages(file.path(where, own), ".")
 if(length(other)!=0)  drat::insertPackages(file.path(dependencies, other), ".")
 
+drat::updateRepo(".", type="source")
+
 # install.packages("osmose", repo="https://osmose-model.github.io/drat/")
 # install.packages("calibrar", repo="https://osmose-model.github.io/drat/")
 # install.packages("lbfgsb3c", repo="https://osmose-model.github.io/drat/")
